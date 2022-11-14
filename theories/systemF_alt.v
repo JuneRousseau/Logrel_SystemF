ca959c10f7c45933ce9027b9030af73b69143718
Bastien Rousseau <bastien.rousseau@ens-rennes.fr>
2022-11-14 17:03:17 +0100

    Alternative syntax (without context) easier to use

--- theories/systemF_alt.v
+++ theories/systemF_alt.v
@@ -80,6 +80,8 @@ End Relation.
   | v_abs : forall x t1, is_val (expr_abs x t1)
   | v_tabs : forall t1, is_val (expr_tabs t1).
 
+  Hint Constructors is_val : core.
+
 
   (** Equality  *)
   (* Lemma to_of_val v : to_val (of_val v) = Some v. *)
@@ -246,54 +248,53 @@ End Relation.
     Δ;Γ ⊢ e _ ∈ <{ [ α := τ1 ] τ2 }>
   where "Δ ; Γ '⊢' t '∈' T" := (typing_judgment Δ Γ t T).
 
+  Hint Constructors typing_judgment : core.
+
   Notation "( x )" := x (in custom sf, x at level 99).
-  Reserved Notation "t '-h->' t'" (at level 60).
-  Inductive pure_step : expr -> expr -> Prop :=
-  | step_fst : forall v1 v2,
-    is_val v1 ->
-    is_val v2 ->
-    <{ fst ⟨ v1, v2 ⟩ }> -h->  <{ v1 }>
-  | step_snd : forall v1 v2,
-    is_val v1 ->
-    is_val v2 ->
-    <{ snd ⟨ v1, v2 ⟩ }> -h->  <{ v2 }>
-  | step_lam : forall x e v,
-    is_val v ->
-    <{ (λ x, e) v }> -h->  <{ [x / v]e }>
-  | step_tlam : forall e,
-    <{ (Λ e) _ }> -h->  <{ e }>
-  where "t '-h->' t'" := (pure_step t t').
-
-  Inductive ectx : Type :=
-  | EmptyCtx : ectx
-  | FstCtx : ectx -> ectx
-  | SndCtx : ectx -> ectx
-  | LPairCtx : ectx -> expr -> ectx
-  | RPairCtx : ectx -> val -> ectx
-  | LAppCtx : ectx -> expr -> ectx
-  | RAppCtx : ectx -> val -> ectx
-  | TAppCtx : ectx -> ectx.
-
-  Fixpoint fill (c : ectx) (e : expr) : expr :=
-    match c with
-    | EmptyCtx => e
-    | FstCtx K => expr_fst (fill K e)
-    | SndCtx K => expr_snd (fill K e)
-    | LPairCtx K t => expr_pair (fill K e) t
-    | RPairCtx K v => expr_pair (of_val v) (fill K e)
-    | LAppCtx K t => expr_app (fill K e) t
-    | RAppCtx K v => expr_app (of_val v) (fill K e)
-    | TAppCtx K => expr_tapp (fill K e)
-    end.
-
   Reserved Notation "t '~>' t'" (at level 60).
-  Inductive step e1 e2 : Prop :=
-  | Step K e1' e2' :
-    e1 = fill K e1' ->
-    e2 = fill K e2' ->
-    e1' -h-> e2' ->
-    e1 ~> e2
-  where "e1 '~>' e2" := (step e1 e2).
+  Inductive step : expr -> expr -> Prop :=
+  | step_fst_red : forall v1 v2,
+    is_val v1 ->
+    is_val v2 ->
+    <{ fst ⟨ v1, v2 ⟩ }> ~>  <{ v1 }>
+  | step_snd_red : forall v1 v2,
+    is_val v1 ->
+    is_val v2 ->
+    <{ snd ⟨ v1, v2 ⟩ }> ~>  <{ v2 }>
+  | step_lam_red : forall x e v,
+    is_val v ->
+    <{ (λ x, e) v }> ~>  <{ [x / v]e }>
+  | step_tlam_red : forall e,
+    <{ (Λ e) _ }> ~>  <{ e }>
+  | step_fst : forall e e',
+    e ~> e' ->
+    <{ fst e }> ~>  <{ fst e' }>
+
+  | step_snd : forall e e',
+    e ~> e' ->
+    <{ snd e }> ~>  <{ snd e' }>
+
+  | step_pairL : forall e1 e1' e2,
+    e1 ~> e1' ->
+    <{ ⟨ e1, e2 ⟩ }> ~>  <{ ⟨ e1', e2 ⟩  }>
+
+  | step_pairR : forall v e e',
+    is_val v ->
+    e ~> e' ->
+    <{ ⟨ v, e ⟩ }> ~>  <{ ⟨ v, e' ⟩  }>
+
+  | step_lam_head : forall f f' e,
+    f ~> f' ->
+    <{ f e }> ~>  <{ f' e }>
+  | step_lam_arg : forall f e e',
+    is_val f ->
+    e ~> e' ->
+    <{ f e }> ~>  <{ f e' }>
+  | step_tlam_head : forall e e',
+    e ~> e' ->
+    <{ e _ }> ~>  <{ e' _ }>
+  where "t '~>' t'" := (step t t').
+  Hint Constructors step : core.
 
   Definition mstep := star expr step.
   Notation "t '~>*' t'" := (mstep t t') (at level 60).
@@ -301,16 +302,11 @@ End Relation.
   (* Definition reducible (e : expr) := *)
   (*   ∃ e', step e e'. *)
 
-  Lemma fill_empty e : fill EmptyCtx e = e.
-  Proof. done. Qed.
-
   (** Examples *)
   Goal <{ (λ x , x) tt }> ~>* <{tt}>.
   Proof.
     eapply star_one.
-    eapply (Step _ _ EmptyCtx) ;
-      [ done | done |].
-    apply step_lam.
+    apply step_lam_red.
     apply v_unit.
   Qed.
 
@@ -318,63 +314,36 @@ End Relation.
   Proof.
     eapply star_trans with <{ fst ( ⟨ (λ y , tt) tt , (λ y , tt) tt⟩ )}>.
     eapply star_one.
-    eapply (Step _ _ (FstCtx EmptyCtx) ).
-    1,2: simpl; done.
-    eapply step_lam; apply v_unit.
+    eapply step_fst; eapply step_lam_red ; apply v_unit.
 
     eapply star_trans with <{ fst ( ⟨ tt , (λ y , tt) tt⟩ )}>.
     eapply star_one.
-    eapply (Step _ _ (FstCtx (LPairCtx EmptyCtx _)) ).
-    1,2: simpl; done.
-    eapply (step_lam y <{ tt }> ); apply v_unit.
+    eapply step_fst; eapply step_pairL; eapply (step_lam_red y <{ tt }> ); apply v_unit.
 
     eapply star_trans with <{ fst ( ⟨ tt , tt⟩ )}>.
     eapply star_one.
-    eapply (Step _ _ (FstCtx (RPairCtx EmptyCtx val_unit)) ).
-    1,2: simpl;done.
-    eapply (step_lam y <{ tt }> ); apply v_unit.
+    eapply step_fst; eapply step_pairR;[apply v_unit|];
+      eapply (step_lam_red y <{ tt }> ); apply v_unit.
 
     eapply star_one.
-    eapply (Step _ _ EmptyCtx).
-    1,2: simpl; done.
-    apply step_fst; apply v_unit.
+    eapply step_fst_red; apply v_unit.
   Qed.
 
   Lemma identity : forall v e, e = (of_val v) -> <{ (λ x , x) e }> ~>* <{e}>.
   Proof.
     intros v e ->.
     eapply star_one.
-    eapply (Step _ _ EmptyCtx).
-    1,2: done.
-    apply step_lam.
+    apply step_lam_red.
     apply is_val_of_val.
   Qed.
-
 
 Lemma is_val_stuck : forall e e', is_val e -> not (e ~> e').
 Proof.
   intros e e' val_e.
   generalize dependent e'.
   induction e; intros e' step; try inversion val_e;inversion step; subst.
-  - destruct K; subst; simpl in *; try discriminate H.
-    rewrite <- H in H1; inversion H1.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    + rewrite <- H3 in H5.
-      inversion H5.
-    + eapply IHe1 with (fill K e2') in H1.
-      apply H1.
-      inversion H3; subst.
-      eapply (Step _ _ K); auto.
-    + eapply IHe2 with (fill K e2') in H2.
-      apply H2.
-      inversion H3; subst.
-      eapply (Step _ _ K); auto.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    rewrite <- H in H3; inversion H3.
-    all:try discriminate H.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    rewrite <- H in H2; inversion H2.
-    all: try discriminate H.
+  - eapply IHe1 in H1; eapply H1; eauto.
+  - eapply IHe2 in H2; eapply H2; eauto.
 Qed.
 
 Lemma is_val_step : forall e e', is_val e -> e ~>* e' -> e' = e.
@@ -382,3 +351,11 @@ Lemma is_val_step : forall e e', is_val e -> e ~>* e' -> e' = e.
   inversion mstep; subst; auto.
   by apply is_val_stuck in H.
 Qed.
+
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
--- theories/type_safety.v
+++ theories/type_safety.v
@@ -1,7 +1,7 @@
 Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
 From Coq Require Import Strings.String.
 From stdpp Require Import gmap list.
-From logical_relation Require Import systemF.
+From logical_relation Require Import systemF_alt.
 
 Definition safe (e: expr) :=
   forall e', e ~>* e' ->
@@ -9,48 +9,6 @@ Definition safe (e: expr) :=
 
 
 (* TODO move to systemF.v, because they are properties of the language *)
-Lemma is_val_step : forall e , is_val e -> e ~>* e.
-Admitted.
-
-Lemma is_val_stuck : forall e e', is_val e -> not (e ~> e').
-Proof.
-  intros e e' val_e.
-  generalize dependent e'.
-  induction e; intros e' step; try inversion val_e;inversion step; subst.
-  - destruct K; subst; simpl in *; try discriminate H.
-    rewrite <- H in H1; inversion H1.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    eapply IHe1 with e2' in H1.
-    admit.
-    inversion H3; subst.
-    admit.
-    admit.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    rewrite <- H in H3; inversion H3.
-    all:try discriminate H.
-  - destruct K; subst; simpl in *; try discriminate H3.
-    rewrite <- H in H2; inversion H2.
-    all: try discriminate H.
-Admitted.
-
-Theorem type_safety :
-  forall e t, nil ; empty ⊢ e ∈ t -> safe e.
-Proof.
-  intros.
-  induction H; unfold safe; intros.
-  - (* unit *)
-    left.
-    inversion H; subst.
-    apply v_unit.
-    apply is_val_stuck in H0; [contradiction|].
-    apply v_unit.
-  - (* var *) admit.
-  - (* pair *) admit.
-  - (* fst *) admit.
-  - (* snd *) admit.
-  - (* abs *) admit.
-  - (* app *)
-Abort.
 
 (** Naive Induction --- Fail *)
 Theorem type_safety :
@@ -60,10 +18,7 @@ Proof.
   induction H; unfold safe; intros.
   - (* unit *)
     left.
-    inversion H; subst.
-    apply v_unit.
-    apply is_val_stuck in H0; [contradiction|].
-    apply v_unit.
+    apply is_val_step in H; subst; apply v_unit.
   - (* var *) admit.
   - (* pair *) admit.
   - (* fst *) admit.
@@ -72,6 +27,213 @@ Proof.
   - (* app *)
 Abort.
 
+Lemma is_val_inversion: forall e, is_val e -> ∃ v, e = (of_val v).
+Proof.
+Admitted.
+
 (** Progress and preservation *)
+Theorem progress : ∀ e t,
+  nil; empty ⊢ e ∈ t ->
+       is_val e ∨ ∃ e', e ~> e'.
+Proof.
+  intros.
+  induction H.
+  - (* unit *) left; apply v_unit.
+  - (* var *) admit.
+  - (* pair *)
+    destruct IHtyping_judgment1, IHtyping_judgment2; [left|right..].
+    + apply v_pair;auto.
+    + destruct H2 as [e2' H2].
+      exists <{ ⟨ e1, e2' ⟩ }>.
+      apply is_val_inversion in H1; destruct H1.
+      rewrite H1.
+      apply step_pairR. apply is_val_of_val. auto.
+    + destruct H1 as [e1' H1].
+      exists <{ ⟨ e1', e2 ⟩ }>.
+      apply step_pairL; auto.
+    + destruct H1 as [e1' H1].
+      exists <{ ⟨ e1', e2 ⟩ }>.
+      apply step_pairL; auto.
+  - (* fst *)
+    destruct IHtyping_judgment; right.
+    + inversion H0; try (rewrite <- H1 in H; inversion H).
+    eexists; apply step_fst_red; auto.
+    + destruct H0 as [e' H0].
+      eexists; eapply step_fst; eauto.
+  - (* snd *)
+    destruct IHtyping_judgment; right.
+    + inversion H0; try (rewrite <- H1 in H; inversion H).
+    eexists; apply step_snd_red; auto.
+    + destruct H0 as [e' H0].
+      eexists; eapply step_snd; eauto.
+  - (* abs *) left; apply v_abs.
+  - (* app *)
+    destruct IHtyping_judgment1, IHtyping_judgment2; right.
+    + (* e has to be λ e' *)
+      inversion H1;
+      match goal with
+      | h: ?val = e |- _ => rewrite <- h in H
+      end;subst ; try inversion H.
+      eexists; apply step_lam_red; auto.
+    + destruct H2 as [e2 H2].
+      eexists; eapply step_lam_arg ; eauto.
+    + destruct H1 as [e1 H1].
+      eexists; eapply step_lam_head ; eauto.
+    + destruct H1 as [e1 H1].
+      eexists; eapply step_lam_head ; eauto.
+  - (* tabs *) left; apply v_tabs.
+  - (* tapp *)
+    destruct IHtyping_judgment; right.
+    + (* e has to be Λ e' *)
+      inversion H0;
+      match goal with
+      | h: ?val = e |- _ => rewrite <- h in H
+      end;subst ; try inversion H.
+      eexists; apply step_tlam_red; auto.
+    + destruct H0 as [e' H0].
+      eexists; eapply step_tlam_head; eauto.
+Admitted.
+
+
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
+
+
+Lemma canonical_form_pair:
+  forall Δ Γ e t1 t2,
+  Δ; Γ ⊢ e ∈ (t1 × t2)
+     -> is_val e
+     -> exists e1 e2, e = <{⟨ e1, e2 ⟩}>.
+Proof.
+  intros * HT val_e.
+  destruct val_e;
+  inversion HT; subst.
+  eexists; eexists; eauto.
+Qed.
+
+Lemma not_free_context_weakening:
+  forall α Γ Γ',
+  Γ ⊆ Γ' →
+  not_free_context α Γ →
+  not_free_context α Γ'.
+Proof.
+  intros.
+  unfold not_free_context in *.
+Admitted.
+
+Lemma weakening : ∀ Δ Δ' Γ Γ' e t,
+  Γ ⊆ Γ' →
+  Δ ; Γ ⊢ e ∈ t →
+  Δ' ; Γ' ⊢ e ∈ t.
+Proof.
+  intros * H Ht.
+  generalize dependent Γ'.
+  generalize dependent Δ'.
+  induction Ht; intros; eauto.
+  - eapply lookup_weaken with (m2 := Γ') in H; eauto.
+  - apply T_Lam. apply IHHt.
+    by apply insert_mono.
+  - apply T_TLam. apply IHHt; eauto.
+    eapply not_free_context_weakening; eauto.
+Qed.
+
+Lemma weakening_empty : ∀ Δ Γ e t, nil ; empty ⊢ e ∈ t -> Δ ; Γ ⊢ e ∈ t.
+Proof.
+  intros.
+  eapply weakening with nil empty.
+  - apply map_empty_subseteq.
+  - done.
+Qed.
+
+Lemma substitution_preserves_typing : ∀ Δ Γ x tx e v t,
+  Δ ; <[x := tx]> Γ ⊢ e ∈ t ->
+  nil ; empty ⊢ v ∈ tx ->
+  Δ ; Γ ⊢ [x/v] e ∈ t.
+Proof.
+  intros * Ht Hv.
+  generalize dependent Γ. generalize dependent t.
+  generalize dependent Δ.
+  induction e; intros * H;
+    (* in each case, we'll want to get at the derivation of H *)
+    inversion H; clear H; subst; simpl; eauto.
+  - (* var *)
+    rename s into y. destruct (x =? y)%string eqn:Heq; subst.
+    + (* x=y *)
+      (* rewrite update_eq in H3. *)
+      (* injection H2 as H2; subst. *)
+      (* apply weakening_empty. assumption. *)
+      admit.
+    + (* x<>y *)
+      apply T_Var.
+      apply eqb_neq in Heq.
+      eapply lookup_insert_ne with (x:=tx) (m := Γ) in Heq.
+      by setoid_rewrite Heq in H3.
+  - (* abs *)
+    admit.
+  - (* tabs *)
+    admit.
+Admitted.
+
+Theorem preservation : ∀ e e' t,
+  nil ; empty ⊢ e ∈ t →
+  e ~> e' →
+  nil ; empty ⊢ e' ∈ t.
+Proof.
+  intros e e' t HTe step.
+  generalize dependent e'.
+  induction HTe; intros.
+  - (* unit *)
+    replace e' with <{ tt }>. apply T_Unit.
+    symmetry; apply (is_val_step <{ tt }> e').
+    apply v_unit.
+    by apply star_one.
+  - (* var *) admit.
+  - (* pair *)
+    inversion step; subst.
+    apply IHHTe1 in H2; apply T_Prod; auto.
+    apply IHHTe2 in H3; apply T_Prod; auto.
+  - (* fst *)
+    inversion step; subst.
+    inversion HTe; subst; auto.
+    apply IHHTe in H0.
+    by eapply T_Fst.
+  - (* snd *)
+    inversion step; subst.
+    inversion HTe; subst; auto.
+    apply IHHTe in H0.
+    by eapply T_Snd.
+  - (* abs *)
+    inversion step; subst.
+  - (* app *)
+    inversion step; subst.
+    + inversion HTe1; subst; auto.
+      admit.
+    + apply IHHTe1 in H2.
+      eapply T_App; eauto.
+    + apply IHHTe2 in H3.
+      eapply T_App; eauto.
+  - (* tabs *)
+    inversion step; subst.
+  - (* tapp *)
+    inversion step; subst.
+    + inversion HTe; subst; auto.
+      admit.
+    + apply IHHTe in H0.
+      eapply T_TApp; eauto.
+Admitted.
+
+Theorem type_safety_progress_preservation:
+  forall e t, nil ; empty ⊢ e ∈ t -> safe e.
+Proof.
+  intros e T HT e' P. induction P.
+  - apply progress in HT. destruct HT; auto.
+  - apply IHP.
+    + apply preservation with (e := a); auto.
+Qed.
 
 (** Logical relation and free theorems --- new files *)
